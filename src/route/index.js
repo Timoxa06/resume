a9a5c5d478c00394d2f03458f72fdd564620e40a
Timoxa06 <tima0957260456@gmail.com>
2023-06-22 13:48:19 +0300

    update

--- src/route/index.js
+++ src/route/index.js
@@ -23,10 +23,152 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    page:{
+      title: 'Resume',
+    },
+    header:{
+      name: {
+        firstname:'Ivan',
+        lastname:'Ivanov'
+      },
+  
+      position: 'Junior Fullstack JS Developer',
+  
+      salary:'600$ в місяц',
+
+      address:'Anna Ahmatova 22',
+    },
+
+    
+    main:{
+      summary:{
+        title:'Summary',
+        text:`Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
+      },
+  
+      experience:{
+          title:'Experience',
+        text:`Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+    
+
+    footer:{
+      social: {
+        email:{
+          text:'dmytro@mail.com',
+          href:'mailto:dmytro@mail.com',
+        },
+        phone:{
+          text:'+380670000123',
+          href:'tel:+380670000123',
+        },
+        linkedin:{
+          text:'LinkedIn',
+          href:'https://www.linkedin.com/in/dmytro-test',
+        },
+      },
+    }
+})
+router.get('/skills', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page:{
+      title: 'Resume',
+    },
+
+    header:{
+      name: {
+        firstname:'Ivan',
+        lastname:'Ivanov'
+      },
+  
+      position: 'Junior Fullstack JS Developer',
+  
+      salary:'600$ в місяц',
+
+      address:'Anna Ahmatova 22',
+    },
+
+    main: {
+      skills: [
+        'HTML',
+        'Handlebars',
+        'VS Code',
+        'GIT',
+        'Terminal',
+        'NPM',
+      ],
+    }, 
+
+    footer:{
+      social: {
+        email:{
+          text:'dmytro@mail.com',
+          href:'mailto:dmytro@mail.com',
+        },
+        phone:{
+          text:'+380670000123',
+          href:'tel:+380670000123',
+        },
+        linkedin:{
+          text:'LinkedIn',
+          href:'https://www.linkedin.com/in/dmytro-test',
+        },
+      },
+    }
   })
 })
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+res.render('education', {
+  // ↙ сюди вводимо JSON дані
+  page:{
+    title: 'Resume',
+  },
 
-// ================================================================
+  header:{
+    name: {
+      firstname:'Ivan',
+      lastname:'Ivanov'
+    },
 
-// Підключаємо роутер до бек-енду
+    position: 'Junior Fullstack JS Developer',
+
+    salary:'600$ в місяц',
+
+    address:'Anna Ahmatova 22'
+  },
+
+  main:{
+    educations:[
+      'Garvard',
+      'hhbfbfd',
+      'Gambridg',
+    ],
+  },
+
+  footer:{
+    social: {
+      email:{
+        text:'dmytro@mail.com',
+        href:'mailto:dmytro@mail.com',
+      },
+      phone:{
+        text:'+380670000123',
+        href:'tel:+380670000123',
+      },
+      linkedin:{
+        text:'LinkedIn',
+        href:'https://www.linkedin.com/in/dmytro-test',
+      },
+    },
+  }
+})
+})
 module.exports = router
